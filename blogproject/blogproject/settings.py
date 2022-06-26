c0ea244c9365a6f75309a94c72e958b703aba352
Sonjeongbeom <sjb990221@gmail.com>
2022-06-26 15:38:19 +0900

    change public IP

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -39,7 +39,9 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "3.39.249.16"
+]
 
 
 # Application definition
